6a4d9cdeaad63bd61f131b70ee1b180a2a3a854f
michael.caulson <mikecaulson111@gmail.com>
2023-07-06 23:55:06 -0600

    Cleaned code and more functionality

--- orbits.c
+++ orbits.c
@@ -17,6 +17,7 @@ All code pertaining to the orbits was original and written by myself
 
 #include "CSCIx229.h"
 #include <time.h>
+#include <stdbool.h>
 
 #define ATan(y,x) (atan2(y,x)*180/3.14159265)
 
@@ -36,30 +37,8 @@ int ph1 = 0;
 int ph2 = 0;
 int th2 = 0;
 
-int inside = 1; // This is the boolean of being inside the house
-
-
-//for textures
-int mode1 = 0;
-
 double asp = 1;
 int fov = 55;
-
-// Many lighting necessities with help from Professor:
-int one       =   1;  // Unit value
-int distance  =   5;  // Light distance
-int inc       =  10;  // Ball increment
-int smooth    =   1;  // Smooth/Flat shading
-int local     =   0;  // Local Viewer Model
-int emission  =   0;  // Emission intensity (%)
-int ambient   =  10;  // Ambient intensity (%)
-int diffuse   =  50;  // Diffuse intensity (%)
-int specular  =   0;  // Specular intensity (%)
-int shininess =   0;  // Shininess (power of two)
-float shiny   =   1;  // Shininess (value)
-int zh        =  90;  // Light azimuth
-float Ylight = 3;
-int sunset = 0; // activates sunset mode
 
 int light = 1;
 
@@ -69,23 +48,10 @@ int h = 500; // height
 // Controls viewing mode:
 int mode = 1; // 0=perspective, 1=Fist Person
 
-double Ex = 0;   // Eye position in the x axis
-double Ez = 9;   // Eye position in the z axis
-double dt = 0.1; // Time step
-
-// To control speed:
-double tvals [10] = {0.001, 0.01, 0.05, 0.1, 0.5, 1, 1.5, 2.0, 2.5, 5.0};
-int tnum = 3; // start at dt = 0.1
-
 int axes = 0;
-
-float eyeposy = 0;
-float accy = -0.001;
-float vely = 0;
 
 const float Emission[]  = {0.0,0.0,0.0,1.0};
 const float Ambient[]   = {0.5,0.5,0.5,1.0};
-// const float Ambient[] = {0.03,0.03,0.03,1.0}; // USE THIS IN THE END
 const float Diffuse[]   = {1.0,1.0,1.0,1.0};
 const float Specular[]  = {0.1,0.1,0.1,1.0};
 const float Direction[] = {0.0,-1.0,0.0,1.0};
@@ -93,26 +59,16 @@ const float Shinyness[] = {16};
 
 float posz = 15.0;
 
+// unsused at this time:
+// unsigned int texture[21];
 
-// View single item at a time
-int single = 4;
-
-// for fun
-int size = 1;
-
-int party = 1;
-
-unsigned int texture[21];
-
-double Cy; // just for now
+//Number of bodies to show:
+int num_bodies = 3;
 
 
 int shader[] = {0,0,0,0,0};
 // changes the shader mode:
 int shademode = 2;
-
-// Controls if lights are swinging
-int swing = 0;
 
 int lines = 0;
 
@@ -166,25 +122,13 @@ float Abs(float x) {
 
 // Using sphere2 from example 8 from class
 static void sphere(double x, double y, double z, double r,int spec) {
-
-    // // float yellow[] = {1,1,0,1};
-    // float Emission[] = {0,0,0,1};
-    // // float Emission[] = {0,0,1,0};
     const int d = 15;
-
-    // glMaterialf(GL_FRONT,GL_SHININESS,shiny);
-    // // glMaterialfv(GL_FRONT,GL_SPECULAR,yellow);
-    // if(spec == 0) {
-    //     glMaterialfv(GL_FRONT,GL_EMISSION,Emission);
-    // }
 
     glPushMatrix();
 
     glTranslated(x,y,z);
     glScaled(r,r,r);
 
-    // glColor3f(1,1,1);
-    // SetColor(1,1,1);
     for(int ph = -90; ph < 90; ph += d) {
         glBegin(GL_QUAD_STRIP);
         for(int th = 0; th <= 360; th += d) {
@@ -223,11 +167,6 @@ typedef struct tag_spheres { // mjc should probably put this at the top
 } spheres;
 // should probably make a create and desytroy function too
 
-double squarer(double number)
-{
-    return number * number;
-}
-
 void update_velocity(spheres *sphere)
 {
     // update veloctiy based on acceleration, then update position
@@ -240,23 +179,6 @@ void update_velocity(spheres *sphere)
     sphere->z += sphere->vz;
 }
 
-double minner(double r1, double r2)
-{
-    if ( r1 < r2 )
-    {
-        return r1;
-    }
-    return r2;
-}
-
-// R in general will have coordinates = m1*r1 + m2*r2 where r1 and r2 are vectors
-typedef struct tag_Rvec {
-    double X;
-    double Y;
-    double Z;
-    double M;
-} Rvec;
-
 int orbit = 0;
 double G = 0.00001;
 
@@ -265,49 +187,14 @@ void update_sphere(spheres *s1, spheres *s2)
 {
     // do update here
 
-    Rvec *R = NULL;
-    R = malloc(sizeof(Rvec));
-
-    // f = ma:
-    // a = f/m, m= 1
-    // a = ( G * m1 * m2 / r^2) / m1
     double x1 = s2->x - s1->x;
     double y1 = s2->y - s1->y;
     double z1 = s2->z - s1->z;
-    // double min = minner(s1->r, s2->r);
-    // // double min = 0.001;
-    // double rx = ( squarer(x1));
-    // double ry = ( squarer(y1));
-    // double rz = ( squarer(z1));
-    // r = sqrt(r);
-
-
 
     double r = sqrt(x1*x1 + y1*y1 + z1*z1);
-    // R->M = s1->m + s2->m;
-    // R->X = (s2->m*s2->x - s1->m*s1->x) / R->M;
-    // R->Y = (s2->m*s2->y - s1->m*s1->y) / R->M;
-    // R->Z = (s2->m*s2->z - s1->m*s1->z) / R->M;
-
-
-
-
 
     if(orbit == 1)
     {
-        // if ( rx > 0.05)
-        // {
-        //     s1->ax = (x1 < 0 ? -1 : 1) * (0.000005 * s2->m / (rx < min ? min : rx)); // G * m2 / r^2
-        // }
-        // if ( ry > 0.05)
-        // {
-        //     s1->ay = (y1 < 0 ? -1 : 1) * (0.000005 * s2->m / (ry < min ? min : ry)); // G * m2 / r^2
-        // }
-        // if ( rz > 0.05)
-        // {
-        //     s1->az = (z1 < 0 ? -1 : 1) * (0.000005 * s2->m / (rz < min ? min : rz)); // G * m2 / r^2
-        // }
-
         s1->ax = G * s2->m * (x1 / (r * r * r));
         s1->ay = G * s2->m * (y1 / (r * r * r));
         s1->az = G * s2->m * (z1 / (r * r * r));
@@ -335,31 +222,29 @@ void Scene2()
     glRotated(th,0,1,0);
 
     glColor3f(1,1,1);
-    // sphere(0,0,5,0.3,0);
-    // // sphere(0,0,5.0,0.3,0);
-    // sphere(2, 0, 0, 0.5, 0);
 
     // spheres here
 
-    for ( int i = 0; i < 2; i++ )
+    for ( int i = 0; i < num_bodies; i++ )
     {
         sphere(all_spheres[i].x, all_spheres[i].y, all_spheres[i].z, all_spheres[i].r, 0);
     }
 
-    for ( int i = 0; i < 2; i++ )
+    for ( int i = 0; i < num_bodies; i++ )
     {
-        for ( int j = 0; j < 2; j++) // can optimize
+        for ( int j = i+1; j < num_bodies; j++)
         {
-            if( i != j )
-            {
-                update_sphere(&all_spheres[i], &all_spheres[j]);
-            }
+            update_sphere(&all_spheres[i], &all_spheres[j]);
+            update_sphere(&all_spheres[j], &all_spheres[i]);
         }
     }
 
     glBegin(GL_LINES);
-    glVertex3d(all_spheres[0].x, all_spheres[0].y, all_spheres[0].z);
-    glVertex3d(all_spheres[1].x, all_spheres[1].y, all_spheres[1].z);
+    for ( int i = 0; i < num_bodies - 1; i++)
+    {
+        glVertex3d(all_spheres[i].x, all_spheres[i].y, all_spheres[i].z);
+        glVertex3d(all_spheres[i+1].x, all_spheres[i+1].y, all_spheres[i+1].z);
+    }
     glEnd();
 
    // Second light for fun
@@ -391,13 +276,8 @@ void Scene2()
     SetColor(1,1,0);
     // glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,red);
     glNormal3f(0,0,1);
-    // glBegin(GL_QUADS);
-    // glVertex2d(-1,-1);
-    // glVertex2d(1,-1);
-    // glVertex2d(1,1);
-    // glVertex2d(-1,1);
-    // glEnd();
-    sphere(0,0,0,0.5,1);
+
+    // sphere(0,0,0,0.5,1);
 
     ErrCheck("Scene");
 
@@ -439,37 +319,30 @@ void special(int key, int x, int y) {
         th -= 15;
         th2-=15;
     } 
-    // else if (key == GLUT_KEY_UP) {
-    //     if(ph < 90) {
-    //         ph += 5;
-    //     }
-    // } else if (key == GLUT_KEY_DOWN) {
-    //     if(ph > -85) {
-    //         ph -= 5;
-    //     }
-    // } 
 
     ph %= 360;
     th %= 360;
 
-    // // if(mode == 0) {
-    // //     Project(0,asp,dim);
-    // // } else {
-    // //     Project(fov,asp,dim);
-    // // }
-    // Project(fov,asp,dim);
     Project(0,asp,dim); // want 2d view so I think this is correct
 
     glutPostRedisplay();
+}
+
+int opposite(int num)
+{
+    int opposite = 0;
+    if ( num == 0)
+    {
+        opposite = 1;
+    }
+
+    return opposite;
 }
 
 void keyDown(unsigned char ch, int x, int y) {
     switch (ch) {
         case 27:
             exit(0);
-            break;
-        case ' ':
-            vely = 0.055;
             break;
         case 'D':
         case 'd':
@@ -482,11 +355,8 @@ void keyDown(unsigned char ch, int x, int y) {
         case 's':
         case 'S':
             posz -= 0.1;
-            printf("%lf\n",posz);
             break;
-            // dim += 0.1;
-            // this does nothing lol
-        case 'p':
+        case ' ':
             if (orbit)
             {
                 orbit = 0;
@@ -501,11 +371,25 @@ void keyDown(unsigned char ch, int x, int y) {
             {
                 orbit = 1;
             }
+            break;
+        case 'u':
+            dim += 1;
+            Project(0,asp,dim);
+            break;
+        case 'y':
+            dim -= 1;
+            Project(0,asp,dim);
+            break;
+        case 'o':
+            axes = opposite(axes);
+            break;
+        case 'l':
+            lines = opposite(lines);
+            break;
     }
 }
 
 void keyUp(unsigned char ch, int x, int y) {
-    // printf("%u",ch);
     switch (ch) {
         case 'D':
         case 'd':
@@ -524,12 +408,6 @@ void reshape(int width,int height)
    //  Set the viewport to the entire window
     asp = (height>0) ? (double)width/height : 1;
     glViewport(0,0, RES*width,RES*height);
-    // Calls the Project to deal with different projections:
-    // if(mode == 0) {
-    //     Project(0,asp,dim);
-    // } else {
-    //     Project(fov,asp,dim);
-    // }
     Project(0,asp,dim);
 
     w = width;
@@ -538,7 +416,6 @@ void reshape(int width,int height)
 
 // Display the scene
 void display() {
-    // glClearColor(0.3,0.5,1.0,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glEnable(GL_DEPTH_TEST);
@@ -552,8 +429,6 @@ void display() {
         } else {
             glWindowPos2i(5,5);
             Print("Angle=%d,%d  Dim=%.1f FOV=%d Projection=%s, k=%d",th,ph,dim,fov,"First Person",shademode);
-            glWindowPos2i(5,20);
-            Print("Cy=%f",Cy);
         }
     }
 
@@ -717,74 +592,111 @@ int main(int argc, char* argv[]) {
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
    //  Create Shader Programs
-//    shader[1] = CreateShaderProg("toon.vert","toon.frag");
-//    shader[2] = CreateShaderProg("pixlight.vert","pixlight.frag");
-//    shader[3] = CreateShaderProg("pixtex.vert","pixtex.frag");
-// //    shader[4] = CreateShaderProg("pixlightspot.vert","pixlightspot.frag");
-//     shader[4] = CreateShaderProg("pixlightspotup.vert","pixlightspotup.frag");
 
 #ifndef USEGLEW
     shader[1] = CreateShaderProg("pixtex.vert","pixtex.frag");
 #endif
-    // shader[2] = CreateShaderProg("pixlightspotup.vert","pixlightspotup.frag");
-    // shader[3] = CreateShaderProg("pixlightspot.vert","pixlightspot.frag");
-
-
-    // Loads Textures
-    // texture[0] = LoadTexBMP("brick.bmp");
-    // texture[0] = LoadTexBMP("brick.bmp");
-    // texture[1] = LoadTexBMP("roof1.bmp");
-    // texture[2] = LoadTexBMP("door.bmp");
-    // texture[3] = LoadTexBMP("newBlanket.bmp");
-    // texture[4] = LoadTexBMP("newOak.bmp");
-    // texture[5] = LoadTexBMP("newercobble.bmp");
-    // texture[6] = LoadTexBMP("stone.bmp");
-    // texture[7] = LoadTexBMP("newbark.bmp");
-    // texture[8] = LoadTexBMP("Steel.bmp");
-    // texture[9] = LoadTexBMP("shutter.bmp");
-    // texture[10] = LoadTexBMP("webb.bmp");
-    // texture[11] = LoadTexBMP("ceiling.bmp");
-    // texture[12] = LoadTexBMP("doodle.bmp");
-    // texture[13] = LoadTexBMP("spookyhouse.bmp");
-    // texture[14] = LoadTexBMP("spookyboy.bmp");
-    // texture[15] = LoadTexBMP("ghost1.bmp");
-    // texture[16] = LoadTexBMP("ghost2.bmp");
-    // texture[17] = LoadTexBMP("ghost3.bmp");
-    // texture[18] = LoadTexBMP("woman.bmp");
-    // texture[19] = LoadTexBMP("exitsign.bmp");
-    // texture[20] = LoadTexBMP("cub.bmp");
 
     for ( int i = 0; i < 10; i++ )
     {
         intiSphere(&all_spheres[i]);
     }
 
-    all_spheres[0].x = 5;
+    // all_spheres[0].x = 5;
+    // all_spheres[0].y = 0;
+    // all_spheres[0].z = 0;
+    // all_spheres[0].r = 0.2;
+    // all_spheres[0].spec = 0;
+    // all_spheres[0].vx = 0;
+    // // all_spheres[0].vy = 100;
+    // all_spheres[0].vy = 0.05;
+    // all_spheres[0].vz = 0.0;
+    // all_spheres[0].ax = 0;
+    // all_spheres[0].ay = 0;
+    // all_spheres[0].az = 0;
+    // all_spheres[0].m = 20;
+
+    // all_spheres[1].x = 0;
+    // all_spheres[1].y = 0;
+    // all_spheres[1].z = 1;
+    // all_spheres[1].r = 0.5;
+    // all_spheres[1].spec = 0;
+    // all_spheres[1].vx = 0;
+    // all_spheres[1].vy = 0;
+    // all_spheres[1].vz = 0;
+    // all_spheres[1].ax = 0;
+    // all_spheres[1].ay = 0;
+    // all_spheres[1].az = 0;
+    // all_spheres[1].m = 4000;
+
+
+    // good for colliding together
+    // all_spheres[0].x = 2;
+    // all_spheres[0].y = 0;
+    // all_spheres[0].z = 0;
+    // all_spheres[0].r = 0.5;
+    // all_spheres[0].spec = 0;
+    // all_spheres[0].vx = 0;
+    // all_spheres[0].vy = 0.01;
+    // all_spheres[0].vz = 0.0;
+    // all_spheres[0].ax = 0;
+    // all_spheres[0].ay = 0;
+    // all_spheres[0].az = 0;
+    // all_spheres[0].m = 200;
+
+    // all_spheres[1].x = -2;
+    // all_spheres[1].y = 0;
+    // all_spheres[1].z = 0;
+    // all_spheres[1].r = 0.5;
+    // all_spheres[1].spec = 0;
+    // all_spheres[1].vx = 0;
+    // all_spheres[1].vy = -0.01;
+    // all_spheres[1].vz = 0;
+    // all_spheres[1].ax = 0;
+    // all_spheres[1].ay = 0;
+    // all_spheres[1].az = 0;
+    // all_spheres[1].m = 200;
+
+
+    // good for nearly perfect orbit, just remove the third set:
+    all_spheres[0].x = 2;
     all_spheres[0].y = 0;
     all_spheres[0].z = 0;
-    all_spheres[0].r = 0.2;
+    all_spheres[0].r = 0.5;
     all_spheres[0].spec = 0;
     all_spheres[0].vx = 0;
-    // all_spheres[0].vy = 100;
-    all_spheres[0].vy = 0.05;
+    all_spheres[0].vy = 0.015;
     all_spheres[0].vz = 0.0;
     all_spheres[0].ax = 0;
     all_spheres[0].ay = 0;
     all_spheres[0].az = 0;
-    all_spheres[0].m = 20;
+    all_spheres[0].m = 200;
 
-    all_spheres[1].x = 0;
+    all_spheres[1].x = -2;
     all_spheres[1].y = 0;
-    all_spheres[1].z = 1;
+    all_spheres[1].z = 0;
     all_spheres[1].r = 0.5;
     all_spheres[1].spec = 0;
     all_spheres[1].vx = 0;
-    all_spheres[1].vy = 0;
+    all_spheres[1].vy = -0.015;
     all_spheres[1].vz = 0;
     all_spheres[1].ax = 0;
     all_spheres[1].ay = 0;
     all_spheres[1].az = 0;
-    all_spheres[1].m = 4000;
+    all_spheres[1].m = 200;
+
+    all_spheres[2].x = 5;
+    all_spheres[2].y = 0;
+    all_spheres[2].z = 0;
+    all_spheres[2].r = 0.2;
+    all_spheres[2].spec = 0;
+    all_spheres[2].vx = 0;
+    all_spheres[2].vy = 0.03;
+    all_spheres[2].vz = 0;
+    all_spheres[2].ax = 0;
+    all_spheres[2].ay = 0;
+    all_spheres[2].az = 0;
+    all_spheres[2].m = 40;
 
 
     Player[0] = 0.0;
